92ae680b3827193babb4770cf639a32de1221f87
小铁 <244974069@qq.com>
2017-01-16 18:09:02 +0800

    Revert "Revert "Revert "初始提交"""
    This reverts commit 2fd492423ec51445479d6a3bd2f2b99c538c0f74.

--- pages/video/video.js
+++
@@ -1,8 +0,0 @@
-var playURl = ['http://pl.youku.com/partner/m3u8?vid=CODMxODIyOA==&type=hd2&ep=CJ5SdhEGxbtIH0rF6wjco0a0VnHYmDy0nfesSKlkt1jWOKyFeVhdtA%3D%3D&sid=748455878053586351832&token=9528&ctype=86&ev=1&oip=2018272572','http://pl.youku.com/partner/m3u8?vid=CMTAxODkyODQ=&type=hd2&ep=CZ5SdhEGxb9HHk7B7wjc9EC0WnTbmDy2jfusSKlGt1jTVKyFc1xftA%3D%3D&sid=648455837117186d31461&token=3108&ctype=86&ev=1&oip=2018272572','http://pl.youku.com/partner/m3u8?vid=CMTA1MjEyNTY=&type=hd2&ep=D55SdhEGxb5HGk3E7AjcoxGxW3fTmDy2jfvlSodot1nDXKyFdl1XtQ%3D%3D&sid=0484558275222863b4559&token=6089&ctype=86&ev=1&oip=2018272572'];
-Page({
-  onLoad: function (options) {
-    this.setData({
-        src: playURl[options.url]
-    })
-  }
-})
